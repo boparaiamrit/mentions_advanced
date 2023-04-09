78eab1dc8f9ca8319c2aa747657fb3efd513fa76
boparaiamrit <boparaiamrit@gmail.com>
2023-04-09 18:33:51 +0530

    Task Comment Model Done

--- lib/flutter_mentions.dart
+++ lib/flutter_mentions.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_portal/flutter_portal.dart';
 
-export 'package:flutter_portal/flutter_portal.dart';
-
 part 'src/annotation_editing_controller.dart';
 part 'src/mention_view.dart';
 part 'src/models.dart';
